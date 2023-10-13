6fe40ef77256a760b9eb5a01924d0c861fba1328
chandraveer.s <chandraveer.s@simformsolutions.com>
2023-10-13 15:02:19 +0530

    fix : designed landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
-
+import leftSideImage from "../assets/img/pexels-fauxels-3184433.png";
+import { Col, Row } from "antd";
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      <Link href={"/overview"}>Dashboard</Link>
-    </div>
+    <>
+      <Row>
+        <Col>
+          <Image src={leftSideImage} alt="left side banner image" />
+        </Col>
+        <Col flex={1} className="justify-content-center col align-items-center">
+          <div>
+            <h1>Welcome to the LMS</h1>
+          </div>
+          <div>
+            <h5>
+              <Link href={"/overview"}>Dashboard</Link>
+            </h5>
+          </div>
+        </Col>
+      </Row>
+    </>
   );
 }
